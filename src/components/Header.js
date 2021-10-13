ddbcad5b6aa35dadcf6cefaddbdc778bd7d8eb3d
Davidcastel26 <davcastellanosalrios@gmail.com>
2021-10-13 17:03:35 -0600

    adding styles;

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,11 +10,13 @@ import propTypes from "prop-types"
 
 const Header = ({title}) => {
     return(
-        <header>
+        <header className='header'>
 {/* in order to make an styles we need to use dooble curly brakes {{}} */}
             {/* <h1 style={{color:'red'}}>{title}</h1> */}
-            {/* if we have an variable we just need to submit the varible name and using one brace {} instead of two */}
-            <h1 style={headingStyle}>{title}</h1>
+{/* if we have an variable we just need to submit the varible name and using one brace {} instead of two */}
+            {/* <h1 style={headingStyle}>{title}</h1> */}
+            <h1> {title} </h1>
+            <button className='btn'> Add </button>
         </header>
     )
 }
@@ -22,10 +24,13 @@ Header.defaultProps = {
     title: 'Task Tracker',
 }
 //if we do not want to have the styles in a line we can create a varible as we did below
-const headingStyle = {
-    color:'red',
-    backgroundColor: 'black'
-}
+//CSS
+// const headingStyle = {
+//     color:'red',
+//     backgroundColor: 'black'
+// }
+//-=-----------------------------------------------------------------------------------
+
 
 Header.propTypes = {
     title: propTypes.string.isRequired,
