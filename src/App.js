407c65f88343e6c38c1a00a9ccca51ebd403118b
Davidcastel26 <davcastellanosalrios@gmail.com>
2021-10-16 17:39:57 -0600

    app done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from './components/AddTask';
+import Footer from './components/Footer';
+import About from './components/About'
 
 
 const App = () => {
@@ -89,26 +92,38 @@ const App = () => {
 
   }
 
-  // const name ='Brad'
-  // const x = true
-  return (
+  // const name ='Brad' ------------------------------------------
+  // const x = true 
+ return (
+  <Router>
     <div className='container'>
       <Header 
         onAdd={() => setShowAddTask(!showAddTask)} 
         showAdd={showAddTask}
       />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ?  <Tasks 
-        tasks={tasks} 
-        onDelete={deleteTask}
-        onToggle={toggleReminder}
-      /> : (
-        'No Tasks to Show at this moment'
-      )}
-      {/* <h1>Hey sup!</h1> */}
-      {/* <h2>hello {name, 1+ 1, '' , x ? 'Yes' : 'No'}</h2> */}
+      
+      <Route path='/' exact render={(props)=> (
+          <>
+            {showAddTask && <AddTask onAdd={addTask} />}
+             {tasks.length > 0 ? ( 
+              <Tasks 
+                tasks={tasks} 
+                onDelete={deleteTask}
+                onToggle={toggleReminder}
+              />) : (
+              'No Tasks to Show at this moment'
+              )
+            }
+             {/* <h1>Hey sup!</h1> */}
+              {/* <h2>hello {name, 1+ 1, '' , x ? 'Yes' : 'No'}</h2> */}
+          </>
+        )}
+      />
+      <Route path='/about' component={About}/>
+      <Footer />
     </div>
-  );
+  </Router>
+ );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,25 +2,30 @@
 // import React from 'react'
 // impt to import a proptype 
 import propTypes from "prop-types"
+import { useLocation } from "react-router-dom"
 import Button from "./Button"
+
 // ORIGINAL PROP
 // const Header = (PROPS ) => {
 // WE COULD USE THE DESTRUCTURE FROM THE PROPS 
 //  <h1>{PROPS.TITLE}</h1>
 
 const Header = ({ title, onAdd, showAdd }) => {
+
+    const location = useLocation()
+
     return(
         <header className='header'>
-{/* in order to make an styles we need to use dooble curly brakes {{}} */}
+{/* in order to make an styles we need to use  dooble curly brakes {{}} */}
             {/* <h1 style={{color:'red'}}>{title}</h1> */}
 {/* if we have an variable we just need to submit the varible name and using one brace {} instead of two */}
             {/* <h1 style={headingStyle}>{title}</h1> */}
             <h1> {title} </h1>
-            <Button 
+            {location.pathname === '/' && ( <Button 
                 color={showAdd ? 'red' :'green'} 
                 text={showAdd ? 'Close' : 'Add'}
                 onClick={onAdd}
-            />
+            /> )} 
         </header>
     )
 }
